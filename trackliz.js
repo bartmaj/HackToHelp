d981f319a7f0cb8a1274c3ad2a5973758c3be760
bmaj <brtmaj@gmail.com>
2015-10-23 12:02:42 -0400

    Add basic css and map

--- trackliz.js
+++ trackliz.js
@@ -1,9 +1,24 @@
 if (Meteor.isClient) {
   //client code
-
+	Meteor.startup(function() {
+    GoogleMaps.load();
+  });
   Template.temp.events({
 	  //event handling
   });
+
+	Template.body.helpers({
+		exampleMapOptions: function() {
+		  // Make sure the maps API has loaded
+		  if (GoogleMaps.loaded()) {
+		    // Map initialization options
+		    return {
+		      center: new google.maps.LatLng(36.850769, -76.285873),
+		      zoom: 12
+		    };
+		  }
+		}
+	});
 }
 
 if (Meteor.isServer) {
@@ -11,3 +26,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
